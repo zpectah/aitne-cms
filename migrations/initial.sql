df9b9018064eca21c1f930f1ba5d0f4ab6cab254
zpecter <sychrat@gmail.com>
2024-09-29 21:48:52 +0200

    sql initial tables

--- migrations/initial.sql
+++ migrations/initial.sql
@@ -184,6 +184,126 @@ LOCK TABLES `cms_categories__en` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `cms_comments`
+--
+
+DROP TABLE IF EXISTS `cms_comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cms_comments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `origin_type` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `origin_id` int NOT NULL,
+  `parent_id` int NOT NULL,
+  `sender` text COLLATE utf8mb4_general_ci NOT NULL,
+  `title` text COLLATE utf8mb4_general_ci NOT NULL,
+  `content` text COLLATE utf8mb4_general_ci NOT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cms_comments`
+--
+
+LOCK TABLES `cms_comments` WRITE;
+/*!40000 ALTER TABLE `cms_comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cms_comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cms_media`
+--
+
+DROP TABLE IF EXISTS `cms_media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cms_media` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text COLLATE utf8mb4_general_ci NOT NULL,
+  `type` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `mime` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `extension` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `active` int NOT NULL,
+  `deleted` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cms_media`
+--
+
+LOCK TABLES `cms_media` WRITE;
+/*!40000 ALTER TABLE `cms_media` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cms_media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cms_members`
+--
+
+DROP TABLE IF EXISTS `cms_members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cms_members` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` text COLLATE utf8mb4_general_ci NOT NULL,
+  `password` text COLLATE utf8mb4_general_ci NOT NULL,
+  `salt` text COLLATE utf8mb4_general_ci NOT NULL,
+  `firstname` text COLLATE utf8mb4_general_ci NOT NULL,
+  `lastname` text COLLATE utf8mb4_general_ci NOT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `active` int NOT NULL,
+  `deleted` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cms_members`
+--
+
+LOCK TABLES `cms_members` WRITE;
+/*!40000 ALTER TABLE `cms_members` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cms_members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cms_pages`
+--
+
+DROP TABLE IF EXISTS `cms_pages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cms_pages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text COLLATE utf8mb4_general_ci NOT NULL,
+  `type` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `categories` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `active` int NOT NULL,
+  `deleted` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cms_pages`
+--
+
+LOCK TABLES `cms_pages` WRITE;
+/*!40000 ALTER TABLE `cms_pages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cms_pages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `cms_settings`
 --
 
@@ -360,4 +480,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-28 16:33:11
+-- Dump completed on 2024-09-29 19:46:36
